dc5621ca7cb2c1e4c4d107087e6f4c9ac60509d1
mosest <mosest@asmsa.org>
2014-01-08 11:24:02 -0600

    i wuv u

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello, World!"); 
 		System.out.println("This is a very low effort program.");
+		System.out.println("I should have picked a funner username.");
 	}
 }
